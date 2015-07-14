318257b0114da7d375178b79a1faa8f2ccf8b095
Georg Richter <georg@mariadb.com>
2015-07-14 06:07:36 +0200

    - Fix for CONC-136: mysql_select_db_start/cont aren't declared in mysql.h - ma_dyncol.h no longer requires longlong declaration from my_global.h

--- include/mysql.h
+++ include/mysql.h
@@ -506,6 +506,8 @@ int STDCALL mysql_autocommit_start(my_bool *ret, MYSQL * mysql,
 int STDCALL mysql_autocommit_cont(my_bool *ret, MYSQL * mysql, int status);
 int STDCALL mysql_next_result_start(int *ret, MYSQL *mysql);
 int STDCALL mysql_next_result_cont(int *ret, MYSQL *mysql, int status);
+int STDCALL mysql_select_db_start(int *ret, MYSQL *mysql, const char *db);
+int STDCALL mysql_select_db_cont(int *ret, MYSQL *mysql, int ready_status);
 int STDCALL mysql_stmt_next_result_start(int *ret, MYSQL_STMT *stmt);
 int STDCALL mysql_stmt_next_result_cont(int *ret, MYSQL_STMT *stmt, int status);
 
