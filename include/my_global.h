b0444f573fa39e83a947b36b421e278a4ab9ab08
Georg Richter <georg@mariadb.com>
2015-09-17 14:15:38 +0200

    Fix for CONC-140: MinGW error due to ssize_t redefinition

--- include/my_global.h
+++ include/my_global.h
@@ -454,7 +454,7 @@ typedef SOCKET_SIZE_TYPE size_socket;
 #ifdef _WIN32
 #define NO_DIR_LIBRARY		/* Not standar dir-library */
 #define USE_MY_STAT_STRUCT	/* For my_lib */
-#ifdef (_MSVC)
+#ifdef _MSVC
 typedef SSIZE_T ssize_t;
 #endif
 #endif
