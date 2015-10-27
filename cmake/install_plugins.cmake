d46a64c4d7502a1b1946b2e5630060fc37fba38b
Georg Richter <georg@mariadb.com>
2015-10-27 16:08:38 +0100

    MSI package fixes (Windows)

--- cmake/install_plugins.cmake
+++ cmake/install_plugins.cmake
@@ -1,13 +1,11 @@
 # plugin installation
 
 MACRO(INSTALL_PLUGIN name binary_dir)
-  IF(NOT WIN32)
-    INSTALL(TARGETS ${name}
-            RUNTIME DESTINATION "${PLUGIN_INSTALL_DIR}"
-            LIBRARY DESTINATION "${PLUGIN_INSTALL_DIR}"
-            ARCHIVE DESTINATION "${PLUGIN_INSTALL_DIR}")
-  ELSE()
-    SET(MARIADB_PLUGINS "${MARIADB_PLUGINS} <File Id=\"${name}.dll\" Name=\"${name}.dll\" DiskId=\"1\" Source=\"${binary_dir}/${CMAKE_BUILD_TYPE}/${name}.dll\"/>\n")
-    MARK_AS_ADVANCED(MARIADB_PLUGINS)
+  INSTALL(TARGETS ${name}
+          RUNTIME DESTINATION "${PLUGIN_INSTALL_DIR}"
+          LIBRARY DESTINATION "${PLUGIN_INSTALL_DIR}"
+          ARCHIVE DESTINATION "${PLUGIN_INSTALL_DIR}")
+  IF(WIN32)
+    FILE(APPEND ${CMAKE_BINARY_DIR}/win/packaging/plugin.conf "<File Id=\"${name}.dll\" Name=\"${name}.dll\" DiskId=\"1\" Source=\"${binary_dir}/${CMAKE_BUILD_TYPE}/${name}.dll\"/>\n")
   ENDIF()
 ENDMACRO()
