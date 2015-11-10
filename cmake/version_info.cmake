b936015139feace80130f4767b2df701230f1133
Georg Richter <georg@mariadb.com>
2015-11-10 17:32:34 +0100

    Fix build outside of ${CMAKE_SOURCE_DIR}: Detecting file version failed if SOURCE != BINARY_DIR

--- cmake/version_info.cmake
+++ cmake/version_info.cmake
@@ -3,7 +3,7 @@ FUNCTION(GET_FILE_VERSION FILE_NAME FILE_VERSION)
   # if we build from a git repository, we calculate the file version:
   #  Patch number is numer of commits for given file
   IF(EXISTS ${CMAKE_SOURCE_DIR}/.git)
-    EXECUTE_PROCESS(COMMAND git --git-dir=${CMAKE_BINARY_DIR}/.git --work-tree=${CMAKE_SOURCE_DIR} rev-list HEAD --count -- ${FILE_NAME} 
+    EXECUTE_PROCESS(COMMAND git --git-dir=${CMAKE_SOURCE_DIR}/.git --work-tree=${CMAKE_SOURCE_DIR} rev-list HEAD --count -- ${FILE_NAME} 
       OUTPUT_VARIABLE FV)
     STRING(REPLACE "\n" "" FV ${FV})
     SET(${FILE_VERSION} ${FV} PARENT_SCOPE)
