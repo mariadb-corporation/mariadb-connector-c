1af54355b41cb2d097e804828099b0a3cfa2c9e1
Georg Richter <georg@mariadb.com>
2015-11-14 11:19:30 +0100

    Fixes for Windows Visual Studio 2015 build

--- include/config-win.h
+++ include/config-win.h
@@ -99,9 +99,16 @@ typedef UINT_PTR rf_SetTimer;
 typedef uint rf_SetTimer;
 #endif
 
+#if (defined(_MSC_VER) && !(_MSC_VER < 1800))
+#define HAVE_STRTOULL 1
+#define HAVE_STRTOLL 1
+#endif
+
 #define Socket_defined
 #define my_socket SOCKET
+#ifndef bool
 #define bool BOOL
+#endif
 #define SIGPIPE	SIGINT
 #define RETQSORTTYPE void
 #define QSORT_TYPE_IS_VOID
