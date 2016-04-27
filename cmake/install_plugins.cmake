56505d8fb62a7b52f978e394aa0f75966cad3832
GeorgyKirichenko <kirichenkoga@gmail.com>
2016-04-27 17:54:07 +0300

    Use PROJECT_SOURCE_DIR instead of CMAKE_SOURCE_DIR. Client library can be used as subdirectory in another cmake project.

--- cmake/install_plugins.cmake
+++ cmake/install_plugins.cmake
@@ -6,6 +6,6 @@ MACRO(INSTALL_PLUGIN name binary_dir)
           LIBRARY DESTINATION "${PLUGIN_INSTALL_DIR}"
           ARCHIVE DESTINATION "${PLUGIN_INSTALL_DIR}")
   IF(WIN32)
-    FILE(APPEND ${CMAKE_BINARY_DIR}/win/packaging/plugin.conf "<File Id=\"${name}.dll\" Name=\"${name}.dll\" DiskId=\"1\" Source=\"${binary_dir}/${CMAKE_BUILD_TYPE}/${name}.dll\"/>\n")
+    FILE(APPEND ${PROJECT_BINARY_DIR}/win/packaging/plugin.conf "<File Id=\"${name}.dll\" Name=\"${name}.dll\" DiskId=\"1\" Source=\"${binary_dir}/${CMAKE_BUILD_TYPE}/${name}.dll\"/>\n")
   ENDIF()
 ENDMACRO()
