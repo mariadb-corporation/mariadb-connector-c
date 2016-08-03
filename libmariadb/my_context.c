9dc2a48a73ad89108f6ad759215271ee6e60fe35
Georg Richter <georg@mariadb.com>
2016-08-03 12:57:33 +0200

    Removed ma_assert stuff

--- libmariadb/my_context.c
+++ libmariadb/my_context.c
@@ -126,7 +126,10 @@ my_context_yield(struct my_context *c)
 int
 my_context_init(struct my_context *c, size_t stack_size)
 {
-  ma_assert(sizeof(char *) == SIZEOF_INT * 2);
+#if SIZEOF_CHARP > SIZEOF_INT*2
+#error Error: Unable to store pointer in 2 ints on this architecture
+#endif
+
   bzero(c, sizeof(*c));
   if (!(c->stack= malloc(stack_size)))
     return -1;                                  /* Out of memory */
