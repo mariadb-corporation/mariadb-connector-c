30614c71e83d639a70d4ed9850b8db06932481e9
Georg Richter <georg@mariadb.com>
2017-02-05 12:00:25 +0100

    Fix for CONC-231: Wrong FSF address

--- libmariadb/mysql_async.c
+++ libmariadb/mysql_async.c
@@ -13,7 +13,7 @@
 
    You should have received a copy of the GNU Library General Public
    License along with this library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 
 /*
   MySQL non-blocking client library functions.
