74b1ba22216a8fc015c45cce87a1848976b31b19
Georg Richter <georg@mariadb.com>
2018-01-08 12:31:33 +0100

    removed unused function char_val from ma_password.c

--- libmariadb/ma_password.c
+++ libmariadb/ma_password.c
@@ -80,13 +80,6 @@ void ma_hash_password(ulong *result, const char *password, size_t len)
   return;
 }
 
-static inline unsigned int char_val(char X)
-{
-  return (uint) (X >= '0' && X <= '9' ? X-'0' :
-		 X >= 'A' && X <= 'Z' ? X-'A'+10 :
-		 X-'a'+10);
-}
-
 /*
  * Genererate a new message based on message and password
  * The same thing is done in client and server and the results are checked.
