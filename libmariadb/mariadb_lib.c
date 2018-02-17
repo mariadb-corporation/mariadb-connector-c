80b2ae25bdc05d3455f28b921d941bf965a7ec8d
Georg Richter <georg@mariadb.com>
2018-02-17 12:22:18 +0100

    If COM_PING failed, check if reconnect option was set before calling mysql_ping again.

--- libmariadb/mariadb_lib.c
+++ libmariadb/mariadb_lib.c
@@ -2539,7 +2539,7 @@ mysql_ping(MYSQL *mysql)
 {
   int rc;
   rc= ma_simple_command(mysql, COM_PING, 0, 0, 0, 0);
-  if (rc && mysql_errno(mysql) == CR_SERVER_LOST)
+  if (rc && mysql->options.reconnect)
     rc= ma_simple_command(mysql, COM_PING, 0, 0, 0, 0);
   return rc;
 }
