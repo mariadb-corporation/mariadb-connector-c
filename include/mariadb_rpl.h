eeeb08106f2226fbaf5d923a5ca9355bd2796b91
Georg Richter <georg@mariadb.com>
2018-05-01 11:38:36 +0200

    added mariadb_rpl_fetch and mariadb_rpl_get_optionsv functions

--- include/mariadb_rpl.h
+++ include/mariadb_rpl.h
@@ -19,7 +19,7 @@
 
 /* Protocol flags */
 #define MARIADB_RPL_DUMP_GTID   (1 << 16)
-#define MARIADB_RPL_SKIP_HEARTBEAT (1 << 17)
+#define MARIADB_RPL_IGNORE_HEARTBEAT (1 << 17)
 
 /* Options */
 enum mariadb_rpl_option {
@@ -32,6 +32,83 @@ enum mariadb_rpl_option {
   MARIADB_RPL_BUFFER
 };
 
+/* Event types: From MariaDB Server sql/log_event.h */
+enum mariadb_rpl_event {
+  UNKNOWN_EVENT= 0,
+  START_EVENT_V3= 1,
+  QUERY_EVENT= 2,
+  STOP_EVENT= 3,
+  ROTATE_EVENT= 4,
+  INTVAR_EVENT= 5,
+  LOAD_EVENT= 6,
+  SLAVE_EVENT= 7,
+  CREATE_FILE_EVENT= 8,
+  APPEND_BLOCK_EVENT= 9,
+  EXEC_LOAD_EVENT= 10,
+  DELETE_FILE_EVENT= 11,
+  NEW_LOAD_EVENT= 12,
+  RAND_EVENT= 13,
+  USER_VAR_EVENT= 14,
+  FORMAT_DESCRIPTION_EVENT= 15,
+  XID_EVENT= 16,
+  BEGIN_LOAD_QUERY_EVENT= 17,
+  EXECUTE_LOAD_QUERY_EVENT= 18,
+  TABLE_MAP_EVENT = 19,
+
+  PRE_GA_WRITE_ROWS_EVENT = 20, /* deprecated */
+  PRE_GA_UPDATE_ROWS_EVENT = 21, /* deprecated */
+  PRE_GA_DELETE_ROWS_EVENT = 22, /* deprecated */
+
+  WRITE_ROWS_EVENT_V1 = 23,
+  UPDATE_ROWS_EVENT_V1 = 24,
+  DELETE_ROWS_EVENT_V1 = 25,
+  INCIDENT_EVENT= 26,
+  HEARTBEAT_LOG_EVENT= 27,
+  IGNORABLE_LOG_EVENT= 28,
+  ROWS_QUERY_LOG_EVENT= 29,
+  WRITE_ROWS_EVENT = 30,
+  UPDATE_ROWS_EVENT = 31,
+  DELETE_ROWS_EVENT = 32,
+  GTID_LOG_EVENT= 33,
+  ANONYMOUS_GTID_LOG_EVENT= 34,
+  PREVIOUS_GTIDS_LOG_EVENT= 35,
+  TRANSACTION_CONTEXT_EVENT= 36,
+  VIEW_CHANGE_EVENT= 37,
+  XA_PREPARE_LOG_EVENT= 38,
+
+  /*
+    Add new events here - right above this comment!
+    Existing events (except ENUM_END_EVENT) should never change their numbers
+  */
+
+  /* New MySQL/Sun events are to be added right above this comment */
+  MYSQL_EVENTS_END,
+
+  MARIA_EVENTS_BEGIN= 160,
+  ANNOTATE_ROWS_EVENT= 160,
+  BINLOG_CHECKPOINT_EVENT= 161,
+  GTID_EVENT= 162,
+  GTID_LIST_EVENT= 163,
+  START_ENCRYPTION_EVENT= 164,
+  QUERY_COMPRESSED_EVENT = 165,
+  WRITE_ROWS_COMPRESSED_EVENT_V1 = 166,
+  UPDATE_ROWS_COMPRESSED_EVENT_V1 = 167,
+  DELETE_ROWS_COMPRESSED_EVENT_V1 = 168,
+  WRITE_ROWS_COMPRESSED_EVENT = 169,
+  UPDATE_ROWS_COMPRESSED_EVENT = 170,
+  DELETE_ROWS_COMPRESSED_EVENT = 171,
+
+  /* Add new MariaDB events here - right above this comment!  */
+
+  ENUM_END_EVENT /* end marker */
+};
+
+struct st_mariadb_rpl_rotate_log_hdr {
+  unsigned long long position;
+  size_t filename_length;
+  const char *filename;
+};
+
 typedef struct st_mariadb_rpl {
   MYSQL *mysql;
   const char *filename;
@@ -41,12 +118,15 @@ typedef struct st_mariadb_rpl {
   unsigned int server_id;
   unsigned long start_position;
   unsigned int flags;
+  union {
+    struct st_mariadb_rpl_rotate_log_hdr rotate_log;
+  } event_hdr;
 } MARIADB_RPL;
 
 /* Function prototypes */
 MARIADB_RPL STDCALL *mariadb_rpl_init(MYSQL *mysql);
-int mariadb_rpl_optionv(MARIADB_RPL *rpl, enum mariadb_rpl_option, ...);
-int mariadb_rpl_get_optionv(MARIADB_RPL *rpl, enum mariadb_rpl_option, ...);
+int mariadb_rpl_optionsv(MARIADB_RPL *rpl, enum mariadb_rpl_option, ...);
+int mariadb_rpl_get_optionsv(MARIADB_RPL *rpl, enum mariadb_rpl_option, ...);
 int STDCALL mariadb_rpl_open(MARIADB_RPL *rpl);
 int STDCALL mariadb_rpl_close(MARIADB_RPL *rpl);
 int STDCALL mariadb_rpl_fetch(MARIADB_RPL *rpl);
--- libmariadb/mariadb_rpl.c
+++ libmariadb/mariadb_rpl.c
@@ -18,10 +18,12 @@
 
 *************************************************************************************/
 
+#include <ma_global.h>
 #include <mysql.h>
 #include <errmsg.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 MARIADB_RPL STDCALL *mariadb_rpl_init(MYSQL *mysql)
 {
@@ -41,7 +43,38 @@ MARIADB_RPL STDCALL *mariadb_rpl_init(MYSQL *mysql)
 
 int STDCALL mariadb_rpl_open(MARIADB_RPL *rpl)
 {
+  unsigned char *ptr, *buf;
   if (!rpl || !rpl->mysql)
+    return 1;
+
+  if (!(rpl->flags & MARIADB_RPL_DUMP_GTID))
+  {
+    /* COM_BINLOG_DUMP:
+       Ofs  Len Data
+       0      1 COM_BINLOG_DUMP
+       1      4 position
+       5      2 flags
+       7      4 server id
+       11     * filename
+
+       * = filename length
+
+     */
+    ptr= buf= (unsigned char *)alloca(rpl->filename_length + 11);
+
+    int4store(ptr, (unsigned int)rpl->start_position);
+    ptr+= 4;
+    int2store(ptr, rpl->flags);
+    ptr+= 2;
+    int4store(ptr, rpl->server_id);
+    ptr+= 4;
+    memcpy(ptr, rpl->filename, rpl->filename_length);
+    ptr+= rpl->filename_length;
+  } else
+  {
+    /* COM_BINLOG_GTID: */
+  }
+  if (ma_simple_command(rpl->mysql, COM_BINLOG_DUMP, (const char *)buf, ptr - buf, 1, 0))
     return 1;
   return 0;
 }
@@ -50,7 +83,40 @@ int STDCALL mariadb_rpl_fetch(MARIADB_RPL *rpl)
 {
   if (!rpl || !rpl->mysql)
     return 1;
-  return 0;
+
+  while (1) {
+    unsigned long pkt_len= ma_net_safe_read(rpl->mysql);
+
+    if (pkt_len == packet_error)
+      return -1;
+
+    /* EOF packet:
+       see https://mariadb.com/kb/en/library/eof_packet/
+       Packet length must be less than 9 bytes, EOF header
+       is 0xFE.
+    */
+    if (pkt_len < 9 && rpl->mysql->net.read_pos[0] == 0xFE)
+    {
+      rpl->buffer_size= 0;
+      return 0;
+    }
+
+    /* if ignore heartbeat flag was set, we ignore this
+       record and continue to fetch next record.
+       The first byte is always status byte (0x00)
+       For event header description see
+       https://mariadb.com/kb/en/library/2-binlog-event-header/ */
+    if (rpl->flags & MARIADB_RPL_IGNORE_HEARTBEAT)
+    {
+      enum mariadb_rpl_event event= rpl->mysql->net.read_pos[1 + 4];
+      if (event == HEARTBEAT_LOG_EVENT)
+        continue;
+    }
+
+    rpl->buffer_size= pkt_len;
+    rpl->buffer = rpl->mysql->net.read_pos;
+    return 0;
+  }
 }
 
 int STDCALL mariadb_rpl_close(MARIADB_RPL *rpl)
@@ -59,20 +125,108 @@ int STDCALL mariadb_rpl_close(MARIADB_RPL *rpl)
     return 1;
   if (rpl->buffer)
     free((void *)rpl->buffer);
+  if (rpl->filename)
+    free((void *)rpl->filename);
   free(rpl);
   return 0;
 }
 
-int mariadb_rpl_optionv(MARIADB_RPL *rpl,
+int mariadb_rpl_optionsv(MARIADB_RPL *rpl,
                         enum mariadb_rpl_option option,
                         ...)
 {
+  va_list ap;
+  int rc= 0;
+
   if (!rpl)
     return 1;
 
+  va_start(ap, option);
+
   switch (option) {
-    default:
-      return -1;
+  case MARIADB_RPL_FILENAME:
+  {
+    char *arg1= va_arg(ap, char *);
+    rpl->filename_length= va_arg(ap, size_t);
+    free((void *)rpl->filename);
+    rpl->filename= NULL;
+    if (rpl->filename_length)
+    {
+      rpl->filename= (const char *)malloc(rpl->filename_length);
+      memcpy((void *)rpl->filename, arg1, rpl->filename_length);
+    }
+    else if (arg1)
+    {
+      rpl->filename= strdup((const char *)arg1);
+      rpl->filename_length= strlen(rpl->filename);
+    }
+    break;
   }
-  return 0;
+  case MARIADB_RPL_SERVER_ID:
+  {
+    rpl->server_id= va_arg(ap, unsigned int);
+    break;
+  }
+  case MARIADB_RPL_FLAGS:
+  {
+    rpl->flags= va_arg(ap, unsigned int);
+    break;
+  }
+  case MARIADB_RPL_START:
+  {
+    rpl->start_position= va_arg(ap, unsigned long);
+    break;
+  }
+  default:
+    rc= -1;
+    goto end;
+  }
+end:
+  return rc;
+}
+
+int mariadb_rpl_get_optionsv(MARIADB_RPL *rpl,
+                        enum mariadb_rpl_option option,
+                        ...)
+{
+  va_list ap;
+  int rc= 0;
+
+  if (!rpl)
+    return 1;
+
+  va_start(ap, option);
+
+  switch (option) {
+  case MARIADB_RPL_FILENAME:
+  {
+    const char **name= va_arg(ap, char **);
+    size_t *len= va_arg(ap, size_t *);
+
+    *name= rpl->filename;
+    *len= rpl->filename_length;
+    break;
+  }
+  case MARIADB_RPL_SERVER_ID:
+  {
+    unsigned int *id= va_arg(ap, unsigned int *);
+    *id= rpl->server_id;
+    break;
+  }
+  case MARIADB_RPL_FLAGS:
+  {
+    unsigned int *flags= va_arg(ap, unsigned int *);
+    *flags= rpl->flags;
+    break;
+  }
+  case MARIADB_RPL_START:
+  {
+    unsigned long *start= va_arg(ap, unsigned long *);
+    *start= rpl->start_position;
+    break;
+  }
+  default:
+    return 1;
+    break;
+  }
 }
