f1f1ece7624595ddc27d65ff33758c729efa828b
Rasmus Johansson <rasmus@mariadb.com>
2018-05-28 11:17:28 +0000

    Added test for building latest Connector/C as client library with latest version of MariaDB Server

---
+++ .travis/libmariadb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# don't pull in submodules. We want the latest C/C as libmariadb
+git config cmake.update-submodules no
+
+# get latest server
+git clone -b 10.3 https://github.com/mariadb/server ../workdir-server
+
+# copy C/C into libmariadb in server
+ls -l ../workdir-server/libmariadb
+cp -r . workdir-server/libmariadb
+
+# build latest server with latest C/C as libmariadb
+# skip to build some storage engines to speed up the build
+cd ../workdir-server
+cmake -DPLUGIN_MROONGA=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_SPIDER=NO -DPLUGIN_TOKUDB=NO
+make -j9
