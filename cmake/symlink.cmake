41cc847c314f4568455229400fb182db1463dfaa
Georg Richter <georg@mariadb.com>
2018-07-04 07:37:03 +0200

    Fix for CONC-346: Removed all OLD cmake policies - Symlink macro now uses cmake generator expression $<TARGET_FILE_NAME:*> - Since all plugins are built in the same directory, the location detection was removed for windows packaging - Installation of the windows *.pdb for libmariadb now also uses a static path.

--- cmake/symlink.cmake
+++ cmake/symlink.cmake
@@ -8,24 +8,14 @@
 MACRO(create_symlink symlink_name target install_path)
 # According to cmake documentation symlinks work on unix systems only
 IF(UNIX)
-  # Get target components 
-  GET_TARGET_PROPERTY(target_location ${target} LOCATION)
-  GET_FILENAME_COMPONENT(target_path ${target_location} PATH)
-  GET_FILENAME_COMPONENT(target_name ${target_location} NAME)
+  # Set target components
+  SET(target_lib $<TARGET_FILE_DIR:${target}>/${symlink_name})
 
   ADD_CUSTOM_COMMAND(
-    OUTPUT ${target_path}/${symlink_name}
-    COMMAND ${CMAKE_COMMAND} ARGS -E remove -f ${target_path}/${symlink_name}
-    COMMAND ${CMAKE_COMMAND} ARGS -E create_symlink ${target_name} ${symlink_name}
-    WORKING_DIRECTORY ${target_path}
-    DEPENDS ${target}
-    )
+    TARGET ${target} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} ARGS -E remove -f ${target_lib}
+    COMMAND ${CMAKE_COMMAND} ARGS -E create_symlink $<TARGET_FILE_NAME:${target}> ${symlink_name})
   
-  ADD_CUSTOM_TARGET(SYM_${symlink_name}
-    ALL
-    DEPENDS ${target_path}/${symlink_name})
-  SET_TARGET_PROPERTIES(SYM_${symlink_name} PROPERTIES CLEAN_DIRECT_OUTPUT 1)
-
   IF(CMAKE_GENERATOR MATCHES "Xcode")
     # For Xcode, replace project config with install config
     STRING(REPLACE "${CMAKE_CFG_INTDIR}" 
@@ -33,7 +23,7 @@ IF(UNIX)
   ENDIF()
 
   # presumably this will be used for libmysql*.so symlinks
-  INSTALL(FILES ${target_path}/${symlink_name} DESTINATION ${install_path}
+  INSTALL(FILES ${target_lib} DESTINATION ${install_path}
           COMPONENT SharedLibraries)
 ENDIF()
 ENDMACRO()
