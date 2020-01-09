5328f70e32740a440f39611eebc035366d827555
Etienne Guesnet <etienne.guesnet.external@atos.net>
2020-01-09 17:10:00 +0100

    AIX specific code no more needed

--- include/ma_global.h
+++ include/ma_global.h
@@ -90,21 +90,6 @@
 #endif /* THREAD */
 
 /* Go around some bugs in different OS and compilers */
-#ifdef _AIX			/* By soren@t.dk */
-#define _H_STRINGS
-#define _SYS_STREAM_H
-#define _AIX32_CURSES
-#define ulonglong2double(A) my_ulonglong2double(A)
-#define my_off_t2double(A)  my_ulonglong2double(A)
-#ifdef	__cplusplus
-extern "C" {
-#endif
-double my_ulonglong2double(unsigned long long A);
-#ifdef	__cplusplus
-}
-#endif
-#endif /* _AIX */
-
 #ifdef HAVE_BROKEN_SNPRINTF	/* HPUX 10.20 don't have this defined */
 #undef HAVE_SNPRINTF
 #endif
