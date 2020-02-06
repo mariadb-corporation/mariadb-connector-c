678f7863260d075fbdb18484d03400bf004f81a7
Lukas Javorsky <ljavorsk@redhat.com>
2020-02-06 15:28:48 +0100

    Make sure that ftell doesn't return negative number
    Covscan log:
    
    Error: NEGATIVE_RETURNS (CWE-394):
    mariadb-connector-c-3.1.6-src/plugins/auth/caching_sha2_pw.c:215: negative_return_fn: Function "ftell(fp)" returns a negative number.
    mariadb-connector-c-3.1.6-src/plugins/auth/caching_sha2_pw.c:215: assign: Assigning: "*pub_key_size" = "ftell(fp)".
    mariadb-connector-c-3.1.6-src/plugins/auth/caching_sha2_pw.c:221: negative_returns: "*pub_key_size" is passed to a parameter that cannot be negative. [Note: The source code implementation of the function has been overridden by a builtin model.]

--- plugins/auth/caching_sha2_pw.c
+++ plugins/auth/caching_sha2_pw.c
@@ -213,6 +213,10 @@ static char *load_pub_key_file(const char *filename, int *pub_key_size)
     goto end;
 
   *pub_key_size= ftell(fp);
+  
+  if (*pub_key_size < 0)
+    goto end;
+
   rewind(fp);
 
   if (!(buffer= malloc(*pub_key_size + 1)))
