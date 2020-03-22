61a3798eaeb9978843f565a2df9b9675cfd0eaec
Georg Richter <georg@mariadb.com>
2020-03-22 17:53:37 +0100

    Fix server build inside travis: Instead of disabling submodules we need to checkout the actual commit.

--- .travis/server-replace-submodule.sh
+++ .travis/server-replace-submodule.sh
@@ -3,16 +3,15 @@
 # get latest server
 git clone -b ${SERVER_BRANCH} https://github.com/mariadb/server ../workdir-server
 
-# copy C/C into libmariadb in server
-ls -l ../workdir-server/libmariadb
-cp -r . ../workdir-server/libmariadb
-
 cd ../workdir-server
 # don't pull in submodules. We want the latest C/C as libmariadb
-git config cmake.update-submodules no
 # build latest server with latest C/C as libmariadb
 # skip to build some storage engines to speed up the build
 cmake -DPLUGIN_MROONGA=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_SPIDER=NO -DPLUGIN_TOKUDB=NO
+cd libmariadb
+git checkout ${TRAVIS_COMMIT}
+cd ..
+git add libmariadb
 make -j9
 
 cd mysql-test/
