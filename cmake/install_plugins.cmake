e242172c3cffd3d9c19594682c00a99bdd094fdb
Vladislav Vaintroub <wlad@mariadb.com>
2020-04-14 20:31:42 +0200

    CMake, Windows - install PDBs for client plugins

--- cmake/install_plugins.cmake
+++ cmake/install_plugins.cmake
@@ -9,6 +9,10 @@
 
 MACRO(INSTALL_PLUGIN name binary_dir)
   INSTALL(TARGETS ${name} COMPONENT ClientPlugins DESTINATION ${INSTALL_PLUGINDIR})
+  IF(MSVC)
+    INSTALL(FILES $<TARGET_PDB_FILE:${name}> COMPONENT Debuginfo
+      DESTINATION symbols CONFIGURATIONS Debug RelWithDebInfo)
+  ENDIF()
   IF(WIN32)
     FILE(APPEND ${CC_BINARY_DIR}/win/packaging/plugin.conf "<File Id=\"${name}.dll\" Name=\"${name}.dll\" DiskId=\"1\" Source=\"${binary_dir}/${CMAKE_BUILD_TYPE}/${name}.dll\"/>\n")
     FILE(APPEND ${CC_BINARY_DIR}/win/packaging/plugin.conf "<File Id=\"${name}.pdb\" Name=\"${name}.pdb\" DiskId=\"1\" Source=\"${binary_dir}/${CMAKE_BUILD_TYPE}/${name}.pdb\"/>\n")
