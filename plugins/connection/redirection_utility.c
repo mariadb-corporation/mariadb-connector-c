15c1d1f7f5edbf614b951e6565785055bf2ebc30
Shihao Chen <shihche@microsoft.com>
2020-06-09 17:36:19 +0800

    rewrite redirection cache as an array(not a list) of cache entries

--- plugins/connection/redirection_utility.c
+++ plugins/connection/redirection_utility.c
@@ -28,6 +28,7 @@
 #include "ma_server_error.h"
 #include <ma_pthread.h>
 #include "redirection_utility.h"
+#include <time.h>
 
 /* Note that key, host and user and malloced together with Redirection_Entry */
 struct Redirection_Entry
@@ -36,8 +37,7 @@ struct Redirection_Entry
 	char* host;
 	char* user;
 	unsigned int port;
-	struct Redirection_Entry* next;
-	struct Redirection_Entry* prev;
+	time_t last_used;
 };
 
 /* Note that host and user are malloced together with Redirection_info */
@@ -48,12 +48,11 @@ struct Redirection_Info
 	unsigned int port;
 };
 
-#define MAX_CACHED_REDIRECTION_ENTRYS 4
+#define MAX_CACHED_REDIRECTION_ENTRIES 4
 #define MAX_REDIRECTION_KEY_LENGTH 512
 #define MAX_REDIRECTION_INFO_LENGTH 512
-static unsigned int redirection_cache_num = 0;
-static struct Redirection_Entry redirection_cache_root = { 0 };
-static struct Redirection_Entry* redirection_cache_rear = NULL;
+
+static struct Redirection_Entry** redirection_cache = NULL;
 
 static pthread_mutex_t redirect_lock;
 
@@ -65,24 +64,29 @@ static pthread_mutex_t redirect_lock;
 
 #define mutex_destroy(x) pthread_mutex_destroy(&x)
 
-int init_redirection_cache()
-{
-	pthread_mutex_init(&redirect_mutex, NULL);
-
-	redirection_cache_root.next = NULL;
-	redirection_cache_rear = &redirection_cache_root;
-	redirection_cache_num = 0;
-
-	return 0;
-}
+my_bool check_redirection_entry(char* key);
 
 void add_redirection_entry(char* key, char* host, char* user, unsigned int port);
 
 void delete_redirection_entry(char* key);
 
-my_bool get_redirection_info(char* key, struct Redirection_Info** info, my_bool copy_on_found);
+struct Redirection_Entry* allocate_redirection_entry(char* key, char* host, char* user, unsigned int port);
+
+struct Redirection_Info* get_redirection_info(char* key);
+
+struct Redirection_Info* allocate_redirection_info(char* host, size_t host_len, char* user, size_t user_len, unsigned int port);
 
 struct Redirection_Info* parse_redirection_info(MYSQL* mysql);
+
+int init_redirection_cache()
+{
+	pthread_mutex_init(&redirect_mutex, NULL);
+
+	redirection_cache = (struct Redirection_Entry**)malloc(sizeof(struct Redirection_Entry*) * MAX_CACHED_REDIRECTION_ENTRIES);
+	memset(redirection_cache, 0, sizeof(struct Redirection_Entry*) * MAX_CACHED_REDIRECTION_ENTRIES);
+
+	return 0;
+}
 
 MYSQL* check_redirect(MYSQL* mysql, const char* host,
 	const char* user, const char* passwd,
@@ -96,15 +100,14 @@ MYSQL* check_redirect(MYSQL* mysql, const char* host,
 	if (redirection_mode != REDIRECTION_OFF)
 	{
 		const char redirect_key[MAX_REDIRECTION_KEY_LENGTH] = { 0 };
-		struct Redirection_Info* info = NULL;
 
 		if (!port)
 			port = MARIADB_PORT;
 
 		snprintf(redirect_key, MAX_REDIRECTION_KEY_LENGTH, "%s_%s_%u", host, user, port);
 
-		my_bool copy_on_found = 1;
-		if (get_redirection_info(redirect_key, &info, copy_on_found))
+		struct Redirection_Info* info = get_redirection_info(redirect_key);
+		if (info)
 		{
 			MYSQL* ret_mysql = mysql->methods->api->mysql_real_connect(mysql,
 				info->host,
@@ -140,7 +143,7 @@ MYSQL* redirect(MYSQL* mysql, const char* host,
 {
 	enable_redirect redirection_mode;
 	mysql->methods->api->mysql_get_option(mysql, MARIADB_OPT_USE_REDIRECTION, &redirection_mode);
-	
+
 	if (redirection_mode == REDIRECTION_OFF)
 		return mysql->methods->db_connect(mysql, host, user, passwd, db, port, unix_socket, client_flag);
 
@@ -224,12 +227,11 @@ MYSQL* redirect(MYSQL* mysql, const char* host,
 
 struct Redirection_Info* parse_redirection_info(MYSQL* mysql)
 {
-	struct Redirection_Info* info = NULL;
 	if (!mysql->info || !mysql->info[0])
 	{
 		return NULL;
 	}
-	
+
 	/*
 		redirection info format:
 		Location: mysql://[redirectedHostName]:redirectedPort/?user=redirectedUser&ttl=%d\n
@@ -287,240 +289,171 @@ struct Redirection_Info* parse_redirection_info(MYSQL* mysql)
 		return NULL;
 	}
 
-	char* host_str = NULL;
-	char* user_str = NULL;
-
-	/* 
-		free(info) will free all pointers alloced by this ma_multi_malloc
-		do not include port_str here because port_str is a temp var, whereas port is the real member
-		of redirection info, so port_str should not get out of this function's scope
-	*/
-	if (!ma_multi_malloc(0,
-		&info, sizeof(struct Redirection_Info),
-		&host_str, (size_t)host_len + 1,
-		&user_str, (size_t)user_len + 1,
-		NULL))
-		return NULL;
-
+	// convert port to integer before allocating redirection info
 	char* port_str = (char*)malloc((size_t)port_len + 1);
-
-	ma_strmake(host_str, host_begin, host_len);
-	ma_strmake(user_str, user_begin, user_len);
 	ma_strmake(port_str, port_begin, port_len);
-	info->host = host_str;
-	info->user = user_str;
-
-	if (!(info->port = strtoul(port_str, NULL, 0)))
-	{
-		free(info);
-		free(port_str);
-		return NULL;
-	}
-
+	unsigned int port = strtoul(port_str, NULL, 0);
 	free(port_str);
+	if (!port)
+		return NULL;
+
+	struct Redirection_Info* info = allocate_redirection_info(host_begin, host_len, user_begin, user_len, port);
 	return info;
 }
 
-my_bool get_redirection_info(char* key, struct Redirection_Info** info, my_bool copy_on_found)
-{
+struct Redirection_Info* get_redirection_info(char* key) {
 	struct Redirection_Entry* ret_entry = NULL;
-	struct Redirection_Entry* cur_entry = NULL;
-
 	mutex_lock(redirect_mutex);
-	cur_entry = redirection_cache_root.next;
-	while (cur_entry)
-	{
-		if (cur_entry->key)
-		{
-			if (!strcmp(cur_entry->key, key))
-			{
-				ret_entry = cur_entry;
-				break;
-			}
-			cur_entry = cur_entry->next;
-		}
-		else break;
+	for (int i = 0; i < MAX_CACHED_REDIRECTION_ENTRIES; ++i) {
+		if (redirection_cache[i] && redirection_cache[i]->key && !strcmp(redirection_cache[i]->key, key))
+			redirection_cache[i]->last_used = time(NULL);
+		ret_entry = redirection_cache[i];
+		break;
 	}
 
-	if (ret_entry)
-	{
-		/* after find the cache, move it to list header */
-		if (redirection_cache_root.next != ret_entry)
-		{
-			if (redirection_cache_rear == ret_entry)
-			{
-				redirection_cache_rear = ret_entry->prev;
-			}
-
-			ret_entry->prev->next = ret_entry->next;
-
-			if (ret_entry->next)
-			{
-				ret_entry->next->prev = ret_entry->prev;
-			}
-
-			ret_entry->next = redirection_cache_root.next;
-			ret_entry->prev = &redirection_cache_root;
-
-			redirection_cache_root.next = ret_entry;
-		}
-
-		if (copy_on_found)
-		{
-			size_t host_len = strlen(ret_entry->host);
-			size_t user_len = strlen(ret_entry->user);
-
-			char* host_str;
-			char* user_str;
-
-			if (!ma_multi_malloc(0,
-				info, sizeof(struct Redirection_Info),
-				&host_str, host_len + 1,
-				&user_str, user_len + 1,
-				NULL))
-			{
-				mutex_unlock(redirect_mutex);
-				return 0;
-			}
-
-			ma_strmake(host_str, ret_entry->host, strlen(ret_entry->host));
-			ma_strmake(user_str, ret_entry->user, strlen(ret_entry->user));
-			(*info)->host = host_str;
-			(*info)->user = user_str;
-			(*info)->port = ret_entry->port;
-		}
+	if (!ret_entry) {
+		mutex_unlock(redirect_mutex);
+		return NULL;
 	}
 
-	my_bool retval = (ret_entry != NULL);
+	struct Redirection_Info* info = allocate_redirection_info(ret_entry->host, strlen(ret_entry->host),
+		ret_entry->user, strlen(ret_entry->user), ret_entry->port);
 	mutex_unlock(redirect_mutex);
-
-	return retval;
+	return info;
 }
 
-void add_redirection_entry(char* key, char* host, char* user, unsigned int port)
+struct Redirection_Info* allocate_redirection_info(char* host, size_t host_len, char* user, size_t user_len, unsigned int port) {
+	struct Redirection_Info* info = NULL;
+
+	char* host_str;
+	char* user_str;
+
+	if (!ma_multi_malloc(0,
+		&info, sizeof(struct Redirection_Info),
+		&host_str, host_len + 1,
+		&user_str, user_len + 1,
+		NULL))
+	{
+		return NULL;
+	}
+
+	ma_strmake(host_str, host, host_len);
+	ma_strmake(user_str, user, user_len);
+	(info)->host = host_str;
+	(info)->user = user_str;
+	(info)->port = port;
+
+	return info;
+}
+
+my_bool check_redirection_entry(char* key) {
+	my_bool found = 0;
+	mutex_lock(redirect_mutex);
+
+	for (int i = 0; i < MAX_CACHED_REDIRECTION_ENTRIES; ++i) {
+		if (redirection_cache[i] && redirection_cache[i]->key && !strcmp(redirection_cache[i]->key, key)) {
+			redirection_cache[i]->last_used = time(NULL);
+			found = 1;
+			break;
+		}
+	}
+
+	mutex_unlock(redirect_mutex);
+	return found;
+}
+
+void add_redirection_entry(char* key, char* host, char* user, unsigned int port) 
 {
-	struct Redirection_Entry* new_entry = NULL;
-	struct Redirection_Entry* popingEntry = NULL;
-	char* _key;
-	char* _host;
-	char* _user;
-	size_t key_len = 0;
-	size_t host_len = 0;
-	size_t user_len = 0;
 
 	if (key == NULL || host == NULL || user == NULL)
 	{
 		return;
 	}
 
-	my_bool copy_on_found = 0;
-	if (get_redirection_info(key, NULL, copy_on_found))
+	if (check_redirection_entry(key))
 	{
 		return;
 	}
 
-	key_len = strlen(key);
-	host_len = strlen(host);
-	user_len = strlen(user);
-
-	if (!ma_multi_malloc(0,
-		&new_entry, sizeof(struct Redirection_Entry),
-		&_key, key_len + 1,
-		&_host, host_len + 1,
-		&_user, user_len + 1,
-		NULL))
-	{
+	struct Redirection_Entry* new_entry = allocate_redirection_entry(key, host, user, port);
+	if (!new_entry) {
 		return;
 	}
 
-	new_entry->key = _key;
-	new_entry->host = _host;
-	new_entry->user = _user;
-	new_entry->next = NULL;
-	new_entry->prev = NULL;
-
-	strcpy(new_entry->key, key);
-	strcpy(new_entry->host, host);
-	strcpy(new_entry->user, user);
-	new_entry->port = port;
-
+	time_t least_recent_use_time = new_entry->last_used;
+	int poping_index = -1;
+	struct Redirection_Entry* poping_entry = NULL;
 	mutex_lock(redirect_mutex);
+	for (int i = 0; i < MAX_CACHED_REDIRECTION_ENTRIES; ++i) {
+		if (redirection_cache[i] == NULL) {
+			redirection_cache[i] = new_entry;
+			mutex_unlock(redirect_mutex);
+			return;
+		}
 
-	if (NULL == redirection_cache_root.next)
-	{
-		redirection_cache_root.next = new_entry;
-		new_entry->prev = &redirection_cache_root;
-		redirection_cache_rear = new_entry;
-	}
-	else
-	{
-		new_entry->next = redirection_cache_root.next;
-		new_entry->prev = &redirection_cache_root;
-
-		redirection_cache_root.next->prev = new_entry;
-		redirection_cache_root.next = new_entry;
+		if (redirection_cache[i]->last_used < least_recent_use_time) {
+			poping_index = i;
+			least_recent_use_time = redirection_cache[i]->last_used;
+		}
 	}
 
-	redirection_cache_num++;
-
-	if (redirection_cache_num > MAX_CACHED_REDIRECTION_ENTRYS)
-	{
-		popingEntry = redirection_cache_rear;
-		redirection_cache_rear = redirection_cache_rear->prev;
-		redirection_cache_rear->next = NULL;
-
-		redirection_cache_num--;
+	if (poping_index != -1) {
+		poping_entry = redirection_cache[poping_index];
+		redirection_cache[poping_index] = new_entry;
 	}
 
 	mutex_unlock(redirect_mutex);
-
-	if (popingEntry)
-		free(popingEntry);
-
-	return;
+	if (poping_entry) {
+		free(poping_entry);
+	}
 }
 
-void delete_redirection_entry(char* key)
-{
+void delete_redirection_entry(char* key) {
 	struct Redirection_Entry* del_entry = NULL;
-	struct Redirection_Entry* cur_entry = NULL;
-
 	mutex_lock(redirect_mutex);
-	cur_entry = redirection_cache_root.next;
-	while (cur_entry)
-	{
-		if (cur_entry->key)
-		{
-			if (!strcmp(cur_entry->key, key))
-			{
-				del_entry = cur_entry;
-				break;
-			}
-			cur_entry = cur_entry->next;
-		}
-		else break;
-	}
 
-	if (del_entry)
-	{
-		if (redirection_cache_rear == del_entry)
-		{
-			redirection_cache_rear = del_entry->prev;
+	for (int i = 0; i < MAX_CACHED_REDIRECTION_ENTRIES; ++i) {
+		if (redirection_cache[i] && redirection_cache[i]->key && !strcmp(redirection_cache[i]->key, key)) {
+			del_entry = redirection_cache[i];
+			redirection_cache[i] = NULL;
+			break;
 		}
-		del_entry->prev->next = del_entry->next;
-
-		if (del_entry->next)
-		{
-			del_entry->next->prev = del_entry->prev;
-		}
-
-		redirection_cache_num--;
 	}
 
 	mutex_unlock(redirect_mutex);
-
 	if (del_entry)
 		free(del_entry);
+}
+
+struct Redirection_Entry* allocate_redirection_entry(char* key, char* host, char* user, unsigned int port) {
+	struct Redirection_Entry* entry = NULL;
+	char* key_str;
+	char* host_str;
+	char* user_str;
+	size_t key_len = strlen(key);
+	size_t host_len = strlen(host);
+	size_t user_len = strlen(user);
+
+	if (!ma_multi_malloc(0,
+		&entry, sizeof(struct Redirection_Entry),
+		&key_str, key_len + 1,
+		&host_str, host_len + 1,
+		&user_str, user_len + 1,
+		NULL))
+	{
+		return NULL;
+	}
+
+	ma_strmake(key_str, key, key_len);
+	ma_strmake(host_str, host, host_len);
+	ma_strmake(user_str, user, user_len);
+	(entry)->key = key_str;
+	(entry)->host = host_str;
+	(entry)->user = user_str;
+	(entry)->port = port;
+	(entry)->last_used = time(NULL);
+
+	return entry;
 }
 
 #ifdef PLUGIN_DYNAMIC
