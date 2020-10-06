6cf8ccc14f5378ea64e36d57261656d61d1dbbb3
Georg Richter <georg@mariadb.com>
2020-10-06 15:30:05 +0200

    Build fix for OpenSSL 1.1.0

--- libmariadb/secure/openssl.c
+++ libmariadb/secure/openssl.c
@@ -355,7 +355,7 @@ static int ma_tls_set_certs(MYSQL *mysql, SSL_CTX *ctx)
         mysql->options.ssl_cipher[0] != 0))
   {
     if(
-#ifdef HAVE_OPENSSL_1_1_API
+#ifdef TLS1_3_VERSION
        SSL_CTX_set_ciphersuites(ctx, mysql->options.ssl_cipher) == 0 &&
 #endif
        SSL_CTX_set_cipher_list(ctx, mysql->options.ssl_cipher) == 0)
