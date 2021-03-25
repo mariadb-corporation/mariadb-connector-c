399d8e9f398665546d30ab233ea1d3a5fb2bbf6c
Rucha Deodhar <rucha.deodhar@mariadb.com>
2021-03-25 21:39:33 +0530

    MDEV-22189: Change error messages inside code to have mariadb instead of mysql
    This patch is made as a part of MDEV-22189 which required changing error
    messages for client.

--- libmariadb/ma_errmsg.c
+++ libmariadb/ma_errmsg.c
@@ -15,7 +15,7 @@
    Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
    MA 02111-1301, USA */
 
-/* Error messages for MySQL clients */
+/* Error messages for clients */
 /* error messages for the demon is in share/language/errmsg.sys */
 
 #include <ma_global.h>
@@ -28,20 +28,20 @@ const char *SQLSTATE_UNKNOWN= "HY000";
 #ifdef GERMAN
 const char *client_errors[]=
 {
-  "Unbekannter MySQL Fehler",
+  "Unbekannter Fehler",
   "Kann UNIX-Socket nicht anlegen (%d)",
-  "Keine Verbindung zu lokalem MySQL Server, socket: '%-.64s' (%d)",
-  "Keine Verbindung zu MySQL Server auf %-.64s (%d)",
+  "Keine Verbindung zu lokalem Server, socket: '%-.64s' (%d)",
+  "Keine Verbindung zu Server auf %-.64s (%d)",
   "Kann TCP/IP-Socket nicht anlegen (%d)",
-  "Unbekannter MySQL Server Host (%-.64s) (%d)",
-  "MySQL Server nicht vorhanden",
+  "Unbekannter Server Host (%-.64s) (%d)",
+  "Server nicht vorhanden",
   "Protokolle ungleich. Server Version = % d Client Version = %d",
-  "MySQL client got out of memory",
+  "Client got out of memory",
   "Wrong host info",
   "Localhost via UNIX socket",
   "%-.64s via TCP/IP",
   "Error in server handshake",
-  "Lost connection to MySQL server during query",
+  "Lost connection to server during query",
   "Commands out of sync; you can't run this command now",
   "Verbindung ueber Named Pipe; Host: %-.64s",
   "Kann nicht auf Named Pipe warten. Host: %-.64s  pipe: %-.32s (%lu)",
@@ -56,20 +56,20 @@ const char *client_errors[]=
 #elif defined PORTUGUESE
 const char *client_errors[]=
 {
-  "Erro desconhecido do MySQL",
+  "Erro desconhecido do",
   "N�o pode criar 'UNIX socket' (%d)",
-  "N�o pode se conectar ao servidor MySQL local atrav�s do 'socket' '%-.64s' (%d)", 
-  "N�o pode se conectar ao servidor MySQL em '%-.64s' (%d)",
+  "N�o pode se conectar ao servidor local atrav�s do 'socket' '%-.64s' (%d)",
+  "N�o pode se conectar ao servidor em '%-.64s' (%d)",
   "N�o pode criar 'socket TCP/IP' (%d)",
-  "'Host' servidor MySQL '%-.64s' (%d) desconhecido", 
-  "Servidor MySQL desapareceu",
+  "'Host' servidor '%-.64s' (%d) desconhecido",
+  "Servidor desapareceu",
   "Incompatibilidade de protocolos. Vers�o do Servidor: %d - Vers�o do Cliente: %d",
-  "Cliente do MySQL com falta de mem�ria",
+  "Cliente do com falta de mem�ria",
   "Informa��o inv�lida de 'host'",
   "Localhost via 'UNIX socket'",
   "%-.64s via 'TCP/IP'",
   "Erro na negocia��o de acesso ao servidor",
-  "Conex�o perdida com servidor MySQL durante 'query'",
+  "Conex�o perdida com servidor durante 'query'",
   "Comandos fora de sincronismo. Voc� n�o pode executar este comando agora",
   "%-.64s via 'named pipe'",
   "N�o pode esperar pelo 'named pipe' para o 'host' %-.64s - 'pipe' %-.32s (%lu)",
@@ -82,20 +82,20 @@ const char *client_errors[]=
 #else /* ENGLISH */
 const char *client_errors[]=
 {
-/* 2000 */  "Unknown MySQL error",
+/* 2000 */  "Unknown error",
 /* 2001 */  "Can't create UNIX socket (%d)",
-/* 2002 */  "Can't connect to local MySQL server through socket '%-.64s' (%d)",
-/* 2003 */  "Can't connect to MySQL server on '%-.64s' (%d)",
+/* 2002 */  "Can't connect to local server through socket '%-.64s' (%d)",
+/* 2003 */  "Can't connect to server on '%-.64s' (%d)",
 /* 2004 */  "Can't create TCP/IP socket (%d)",
-/* 2005 */  "Unknown MySQL server host '%-.100s' (%d)",
-/* 2006 */  "MySQL server has gone away",
+/* 2005 */  "Unknown server host '%-.100s' (%d)",
+/* 2006 */  "server has gone away",
 /* 2007 */  "Protocol mismatch. Server Version = %d Client Version = %d",
-/* 2008 */  "MySQL client run out of memory",
+/* 2008 */  "Client run out of memory",
 /* 2009 */  "Wrong host info",
 /* 2010 */  "Localhost via UNIX socket",
 /* 2011 */  "%-.64s via TCP/IP",
 /* 2012 */  "Error in server handshake",
-/* 2013 */  "Lost connection to MySQL server during query",
+/* 2013 */  "Lost connection to server during query",
 /* 2014 */  "Commands out of sync; you can't run this command now",
 /* 2015 */  "%-.64s via named pipe",
 /* 2016 */  "Can't wait for named pipe to host: %-.64s  pipe: %-.32s (%lu)",
@@ -137,7 +137,7 @@ const char *client_errors[]=
 /* 2052 */  "Prepared statement contains no metadata",
 /* 2053 */  "",
 /* 2054 */  "This feature is not implemented or disabled",
-/* 2055 */  "Lost connection to MySQL server at '%s', system error: %d",
+/* 2055 */  "Lost connection to server at '%s', system error: %d",
 /* 2056 */  "Server closed statement due to a prior %s function call",
 /* 2057 */  "The number of parameters in bound buffers differs from number of columns in resultset",
 /* 2059 */  "Can't connect twice. Already connected",
