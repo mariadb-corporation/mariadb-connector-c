1ef1428df8fa30e7574fd5e07f252f3730cd0861
kolzeq <diego.dupin@gmail.com>
2021-06-16 10:26:30 +0200

    Pull request handling

--- travis.sh
+++ travis.sh
@@ -22,11 +22,15 @@ if [ -n "$server_branch" ] ; then
   # skip to build some storage engines to speed up the build
   cmake -DPLUGIN_MROONGA=NO -DPLUGIN_ROCKSDB=NO -DPLUGIN_SPIDER=NO -DPLUGIN_TOKUDB=NO
   cd libmariadb
-  if [ -z "$TRAVIS_PULL_REQUEST" ] ; then
+    echo "PR:${TRAVIS_PULL_REQUEST} TRAVIS_COMMIT:${TRAVIS_COMMIT}"
+  if [ -n "$TRAVIS_PULL_REQUEST" ] && [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     # fetching pull request
+    echo "fetching PR"
     git fetch origin pull/${TRAVIS_PULL_REQUEST}/head:PR_${TRAVIS_PULL_REQUEST}
+    echo "checkout PR"
     git checkout PR_${TRAVIS_PULL_REQUEST}
   else
+    echo "checkout commit"
     git checkout ${TRAVIS_COMMIT}
   fi
 
