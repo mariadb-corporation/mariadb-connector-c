9c651bd6789e87c9f0daeb05bf2238f5497ea37b
Georg Richter <georg@mariadb.com>
2021-07-26 17:47:37 +0200

    CONC-565: Incorrect max_length in binary protocol
    Fixed max_length values in ma_stmt_codecs for integer types:
    - all types included an extra byte for trailing zero character
    - maximum value for year column type is 2155 (and not 0xFFFF).

--- libmariadb/ma_stmt_codec.c
+++ libmariadb/ma_stmt_codec.c
@@ -1240,27 +1240,27 @@ void mysql_init_ps_subsystem(void)
 
   mysql_ps_fetch_functions[MYSQL_TYPE_TINY].func    = ps_fetch_int8;
   mysql_ps_fetch_functions[MYSQL_TYPE_TINY].pack_len  = 1;
-  mysql_ps_fetch_functions[MYSQL_TYPE_TINY].max_len  = 4;
+  mysql_ps_fetch_functions[MYSQL_TYPE_TINY].max_len  = 3;
 
   mysql_ps_fetch_functions[MYSQL_TYPE_SHORT].func    = ps_fetch_int16;
   mysql_ps_fetch_functions[MYSQL_TYPE_SHORT].pack_len  = 2;
-  mysql_ps_fetch_functions[MYSQL_TYPE_SHORT].max_len  = 6;
+  mysql_ps_fetch_functions[MYSQL_TYPE_SHORT].max_len  = 5;
 
   mysql_ps_fetch_functions[MYSQL_TYPE_YEAR].func    = ps_fetch_int16;
   mysql_ps_fetch_functions[MYSQL_TYPE_YEAR].pack_len  = 2;
-  mysql_ps_fetch_functions[MYSQL_TYPE_YEAR].max_len  = 6;
+  mysql_ps_fetch_functions[MYSQL_TYPE_YEAR].max_len  = 4;
 
   mysql_ps_fetch_functions[MYSQL_TYPE_INT24].func    = ps_fetch_int32;
   mysql_ps_fetch_functions[MYSQL_TYPE_INT24].pack_len  = 4;
-  mysql_ps_fetch_functions[MYSQL_TYPE_INT24].max_len  = 9;
+  mysql_ps_fetch_functions[MYSQL_TYPE_INT24].max_len  = 8;
 
   mysql_ps_fetch_functions[MYSQL_TYPE_LONG].func    = ps_fetch_int32;
   mysql_ps_fetch_functions[MYSQL_TYPE_LONG].pack_len  = 4;
-  mysql_ps_fetch_functions[MYSQL_TYPE_LONG].max_len  = 11;
+  mysql_ps_fetch_functions[MYSQL_TYPE_LONG].max_len  = 10;
 
   mysql_ps_fetch_functions[MYSQL_TYPE_LONGLONG].func  = ps_fetch_int64;
   mysql_ps_fetch_functions[MYSQL_TYPE_LONGLONG].pack_len= 8;
-  mysql_ps_fetch_functions[MYSQL_TYPE_LONGLONG].max_len  = 21;
+  mysql_ps_fetch_functions[MYSQL_TYPE_LONGLONG].max_len  = 20;
 
   mysql_ps_fetch_functions[MYSQL_TYPE_FLOAT].func    = ps_fetch_float;
   mysql_ps_fetch_functions[MYSQL_TYPE_FLOAT].pack_len  = 4;
