b973c75c27d92d519a6db8831bea1de3da13bbc8
Georg Richter <georg@mariadb.com>
2022-04-20 18:46:15 +0200

    CONC-580: Fixed license header
    Moved ref10/common.h to plugins/auth/ed25519_common.h and fixed
    license header

--- plugins/auth/ed25519_common.h
+++ plugins/auth/ed25519_common.h
@@ -17,7 +17,6 @@
    51 Franklin St., Fifth Floor, Boston, MA 02110, USA
 *************************************************************************************/
 
-
 #include <mysql.h>
 #include <string.h>
 
