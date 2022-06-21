c0fea17e2a05d636bbea980be73ba8e67361ee07
hyunghwan.chung <hyunghwan.chung@gmail.com>
2022-06-21 17:49:45 +0900

    enhanced mysql_close() and other related parts to prevent memory leaks when terminating an initiated but unestablished connection

--- plugins/pvio/pvio_socket.c
+++ plugins/pvio/pvio_socket.c
@@ -733,6 +733,16 @@ int pvio_socket_fast_send(MARIADB_PVIO *pvio)
 }
 
 static int
+pvio_socket_connect_async(MARIADB_PVIO *pvio,
+                          const struct sockaddr *name, uint namelen)
+{
+  MYSQL *mysql= pvio->mysql;
+  mysql->options.extension->async_context->pvio= pvio;
+  pvio_socket_blocking(pvio, 0, 0);
+  return my_connect_async(pvio, name, namelen, pvio->timeout[PVIO_CONNECT_TIMEOUT]);
+}
+
+static int
 pvio_socket_connect_sync_or_async(MARIADB_PVIO *pvio,
                           const struct sockaddr *name, uint namelen)
 {
@@ -742,9 +752,7 @@ pvio_socket_connect_sync_or_async(MARIADB_PVIO *pvio,
   {
     /* even if we are not connected yet, application needs to check socket
      * via mysql_get_socket api call, so we need to assign pvio */
-    mysql->options.extension->async_context->pvio= pvio;
-    pvio_socket_blocking(pvio, 0, 0);
-    return my_connect_async(pvio, name, namelen, pvio->timeout[PVIO_CONNECT_TIMEOUT]);
+    return pvio_socket_connect_async(pvio, name, namelen);
   }
 
   return pvio_socket_internal_connect(pvio, name, namelen);
@@ -916,7 +924,18 @@ my_bool pvio_socket_connect(MARIADB_PVIO *pvio, MA_PVIO_CINFO *cinfo)
         }
       }
 
-      rc= pvio_socket_connect_sync_or_async(pvio, save_res->ai_addr, (uint)save_res->ai_addrlen);
+      if (mysql->options.extension && mysql->options.extension->async_context &&
+          mysql->options.extension->async_context->active)
+      {
+        mysql->options.extension->async_context->pending_gai_res = res;
+        rc= pvio_socket_connect_async(pvio, save_res->ai_addr, (uint)save_res->ai_addrlen);
+        mysql->options.extension->async_context->pending_gai_res = NULL;
+      }
+      else
+      {
+        rc= pvio_socket_connect_sync_or_async(pvio, save_res->ai_addr, (uint)save_res->ai_addrlen);
+      }
+
       if (!rc)
       {
         MYSQL *mysql= pvio->mysql;
