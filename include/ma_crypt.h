1b3cf6b441cb70e7538de0f4af36ef8107b43847
Vladislav Vaintroub <vvaintroub@gmail.com>
2023-09-21 13:36:23 +0200

    CONC-669 Cache bcrypt algorithm providers in win_crypt.c
    CryptOpenAlgorithmProvider() documentation states:
    "We recommend that you cache any algorithm provider handles that you will
    use more than once, rather than opening and closing the algorithm providers
    over and over."
    
    This patch implements algorithm handle caching,
    and also simplifies how hashes are handled in win_crypt.c
    
    - MA_HASH_CTX* is now just BCrypt hash handle.
    - We no longer allocate or free memory any memory outselves.
    
    There are small change in the ma_hash API:
    ma_hash_new() lost the second parameter, only used on Windows previously

--- include/ma_crypt.h
+++ include/ma_crypt.h
@@ -31,6 +31,7 @@
 #define MA_HASH_SHA384    5
 #define MA_HASH_SHA512    6
 #define MA_HASH_RIPEMD160 7
+#define MA_HASH_MAX       8
 
 /*! Hash digest sizes */
 #define MA_MD5_HASH_SIZE 16
@@ -45,15 +46,7 @@
 /** \typedef MRL hash context */
 
 #if defined(_WIN32)
-#include <windows.h>
-#include <bcrypt.h>
-typedef struct {
-  char free_me;
-  BCRYPT_ALG_HANDLE hAlg;
-  BCRYPT_HASH_HANDLE hHash;
-  PBYTE hashObject;
-  DWORD digest_len;
-} MA_HASH_CTX;
+typedef void MA_HASH_CTX;
 #elif defined(HAVE_OPENSSL)
 typedef void MA_HASH_CTX;
 #elif defined(HAVE_GNUTLS)
@@ -67,11 +60,10 @@ typedef struct {
   @brief acquire and initialize new hash context
 
   @param[in] algorithm   hash algorithm
-  @param[in] ctx         pointer to a crypto context
 
   @return    hash context on success, NULL on error
 */
-MA_HASH_CTX *ma_hash_new(unsigned int algorithm, MA_HASH_CTX *ctx);
+MA_HASH_CTX *ma_hash_new(unsigned int algorithm);
 
 /**
   @brief release and deinitializes a hash context
@@ -153,11 +145,7 @@ static inline void ma_hash(unsigned int algorithm,
                            unsigned char *digest)
 {
   MA_HASH_CTX *ctx= NULL;
-#ifdef _WIN32
-  MA_HASH_CTX dctx;
-  ctx= &dctx;
-#endif
-  ctx= ma_hash_new(algorithm, ctx);
+  ctx= ma_hash_new(algorithm);
   ma_hash_input(ctx, buffer, buffer_length);
   ma_hash_result(ctx, digest);
   ma_hash_free(ctx);
