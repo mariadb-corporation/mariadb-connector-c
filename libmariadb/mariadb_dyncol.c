dbc84229e2789fe3e524547f20ea4203781dd10f
Daniel Black <daniel@mariadb.org>
2025-08-21 13:19:41 +1000

    CONC-786: dyncol: hdr.offset is read without initialization
    The init_read_hdr static function is called from 3
    locations within this file for a hdr on the stack.
    This was the logical location to initialize offset.
    
    Infer error:
    
    libmariadb/mariadb_dyncol.c:4145: error: Uninitialized Value(PULSE_UNINITIALIZED_VALUE)
      `hdr.offset` is read without initialization during the call to `hdr_interval_length()`.
      4143.       goto err;
      4144.     header.length=
      4145.       hdr_interval_length(&header, header.entry + header.entry_size);
                  ^
      4146.     header.data= header.dtpool + header.offset;
      4147.     /*

--- libmariadb/mariadb_dyncol.c
+++ libmariadb/mariadb_dyncol.c
@@ -675,6 +675,7 @@ init_read_hdr(DYN_HEADER *hdr, DYNAMIC_COLUMN *str)
   if (read_fixed_header(hdr, str))
     return ER_DYNCOL_FORMAT;
   hdr->header= (uchar*)str->str + fmt_data[hdr->format].fixed_hdr;
+  hdr->offset= 0;
   calc_param(&hdr->entry_size, &hdr->header_size,
              fmt_data[hdr->format].fixed_hdr_entry, hdr->offset_size,
              hdr->column_count);
