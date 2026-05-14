cc145b5cae4cff471152d93edf213c0134bbc21f
ParadoxV5 <paradox.ver5@gmail.com>
2026-05-13 19:26:11 -0600

    Fix redefining `bool` when including `ma_global.h` in C++
    If `HAVE_BOOL` is not defined (by default), the C++ check didn’t matter because of the `||` operator.

--- include/ma_global.h
+++ include/ma_global.h
@@ -687,7 +687,7 @@ typedef unsigned long	size_s; /* Size of strings (In string-funcs) */
 typedef int		myf;	/* Type of MyFlags in my_funcs */
 typedef char		my_bool; /* Small bool */
 typedef unsigned long long my_ulonglong;
-#if !defined(bool) && !defined(bool_defined) && (!defined(HAVE_BOOL) || !defined(__cplusplus))
+#if !defined(bool) && !defined(bool_defined) && !defined(HAVE_BOOL) && !defined(__cplusplus)
 typedef char		bool;	/* Ordinary boolean values 0 1 */
 #endif
 	/* Macros for converting *constants* to the right type */
