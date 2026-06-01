37b8818ead817cbb371da5dca3c2cec86f8668f5
ParadoxV5 <paradox.ver5@gmail.com>
2026-06-01 16:55:19 -0600

    GitHub web UI didn’t save my conflict resolution

--- include/ma_global.h
+++ include/ma_global.h
@@ -685,11 +685,7 @@ typedef unsigned long	size_s; /* Size of strings (In string-funcs) */
 typedef int		myf;	/* Type of MyFlags in my_funcs */
 typedef char		my_bool; /* Small bool */
 typedef unsigned long long my_ulonglong;
-<<<<<<< cpp_bool
-#if !defined(bool) && !defined(bool_defined) && !defined(HAVE_BOOL) && !defined(__cplusplus)
-=======
-#if !defined(bool) && !defined(bool_defined) && (!defined(HAVE_BOOL) || !defined(__cplusplus)) && (__STDC_VERSION__ < 202300L)
->>>>>>> 3.3
+#if !defined(bool) && !defined(bool_defined) && !defined(HAVE_BOOL) && !defined(__cplusplus) && (__STDC_VERSION__ < 202300L)
 typedef char		bool;	/* Ordinary boolean values 0 1 */
 #endif
 	/* Macros for converting *constants* to the right type */
