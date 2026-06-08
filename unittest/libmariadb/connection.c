af9eba34d34616221a4a9b1e0a4b5dc48d6fcec0
Georg Richter <georg@mariadb.com>
2026-06-08 12:32:56 +0200

    Replace pyopenssl by native crypto

--- unittest/libmariadb/connection.c
+++ unittest/libmariadb/connection.c
@@ -841,8 +841,15 @@ static int test_bind_address(MYSQL *my)
     return SKIP;
   }
 
-  sprintf(query, "DROP USER '%s'@'%s'", username, bind_addr);
+  if (!bind_addr)
+  {
+    diag("No bind address specified");
+    return SKIP;
+  }
+
+  sprintf(query, "DROP USER IF EXISTS '%s'@'%s'", username, bind_addr);
   rc= mysql_query(my, query);
+  check_mysql_rc(rc, my);
 
   sprintf(query, "CREATE USER '%s'@'%s' IDENTIFIED BY '%s'", username, bind_addr, password);
   rc= mysql_query(my, query);
@@ -851,12 +858,6 @@ static int test_bind_address(MYSQL *my)
   sprintf(query, "GRANT ALL ON %s.* TO '%s'@'%s'", schema, username, bind_addr);
   rc= mysql_query(my, query);
   check_mysql_rc(rc, my);
-
-  if (!bind_addr)
-  {
-    diag("No bind address specified");
-    return SKIP;
-  }
 
   mysql= mysql_init(NULL);
   mysql_options(mysql, MYSQL_OPT_BIND, bind_addr);
--- unittest/libmariadb/tls_server.py
+++ unittest/libmariadb/tls_server.py
@@ -2,7 +2,12 @@ import socket
 import ssl
 import argparse
 from ast import literal_eval
-from OpenSSL import crypto, SSL
+# from OpenSSL import crypto, SSL
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from datetime import datetime, timedelta, UTC
 import os
 
 class TlsServer():
@@ -11,10 +16,12 @@ class TlsServer():
         self.host= kwargs.pop("host", "127.0.0.1")
         self.port= kwargs.pop("port", 50000)
         self.server= None
+        self.context= None
         self.end= False
 
         try:
             self.server= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.server.bind((self.host, self.port))
             print("# tls dummy_server started: ", self.host, self.port)
             self.server.listen()
@@ -25,7 +32,7 @@ class TlsServer():
     def check_server(self):
         if not self.server:
             raise Exception("Server not started")
-             
+
 
     def send_server_hello(self, conn):
         self.check_server()
@@ -38,63 +45,118 @@ class TlsServer():
         return 1
 
     def generate_cert(self,
-                      create_new=False,
-                      create_crl=False,
-                      emailAddress="emailAddress",
-                      commonName="commonName",
-                      SAN=None,
-                      countryName="NT",
-                      localityName="localityName",
-                      stateOrProvinceName="stateOrProvinceName",
-                      organizationName="organizationName",
-                      organizationUnitName="organizationUnitName",
-                      serialNumber=123,
-                      validityStartInSeconds=0,
-                      validityEndInSeconds=10*365*24*60*60,
-                      KEY_FILE = "privkey.pem",
-                      CRL_FILE = "selfsigned.crl",
-                      CERT_FILE="selfsigned.pem"):
+                  create_new=False,
+                  create_crl=False,
+                  emailAddress="emailAddress",
+                  commonName="commonName",
+                  SAN=None,
+                  countryName="NT",
+                  localityName="localityName",
+                  stateOrProvinceName="stateOrProvinceName",
+                  organizationName="organizationName",
+                  organizationUnitName="organizationUnitName",
+                  serialNumber=123,
+                  validityStartInSeconds=0,
+                  validityEndInSeconds=10*365*24*60*60,
+                  KEY_FILE="privkey.pem",
+                  CRL_FILE="selfsigned.crl",
+                  CERT_FILE="selfsigned.pem"):
 
-
-        self.key_file= KEY_FILE
-        self.cert_file= CERT_FILE
+        self.key_file = KEY_FILE
+        self.cert_file = CERT_FILE
         self.crl_file = CRL_FILE
 
-        if create_new:
-            try:
-                k = crypto.PKey()
-                k.generate_key(crypto.TYPE_RSA, 4096)
-                # create a self-signed cert
-                cert = crypto.X509()
-                cert.get_subject().C = countryName
-                cert.get_subject().ST = stateOrProvinceName
-                cert.get_subject().L = localityName
-                cert.get_subject().O = organizationName
-                cert.get_subject().OU = organizationUnitName
-                cert.get_subject().CN = commonName
-                cert.get_subject().emailAddress = emailAddress
-                cert.set_serial_number(serialNumber)
-                cert.gmtime_adj_notBefore(validityStartInSeconds)
-                cert.gmtime_adj_notAfter(validityEndInSeconds)
-                cert.set_issuer(cert.get_subject())
-                if SAN:
-                   print(SAN)
-                   san_list= [SAN,]
-                   cert.add_extensions([
-                      crypto.X509Extension(
-                      b"subjectAltName", False, "," . join(san_list).encode()
-                   )])
-                cert.set_pubkey(k)
-                cert.sign(k, 'sha512')
-                with open(CERT_FILE, "wt") as f:
-                    f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8"))
-                with open(KEY_FILE, "wt") as f:
-                    f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8"))
-                return 1
-            except Exception as e:
-                return 0
-        return 1
+        if not create_new:
+            return 1
 
+        try:
+            key = rsa.generate_private_key(
+                public_exponent=65537,
+                key_size=4096,
+            )
+
+            subject = issuer = x509.Name([
+                x509.NameAttribute(NameOID.COUNTRY_NAME, countryName),
+                x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME,
+                                   stateOrProvinceName),
+                x509.NameAttribute(NameOID.LOCALITY_NAME,
+                                   localityName),
+                x509.NameAttribute(NameOID.ORGANIZATION_NAME,
+                                   organizationName),
+                x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME,
+                                   organizationUnitName),
+                x509.NameAttribute(NameOID.COMMON_NAME,
+                                   commonName),
+                x509.NameAttribute(NameOID.EMAIL_ADDRESS,
+                                   emailAddress),
+            ])
+
+            now = datetime.now(UTC)
+
+            cert_builder = (
+                x509.CertificateBuilder()
+                .subject_name(subject)
+                .issuer_name(issuer)
+                .public_key(key.public_key())
+                .serial_number(serialNumber)
+                .not_valid_before(
+                    now + timedelta(seconds=validityStartInSeconds)
+                )
+                .not_valid_after(
+                    now + timedelta(seconds=validityEndInSeconds)
+                )
+            )
+
+            if SAN:
+                san_entries = []
+
+                for item in SAN.split(","):
+                    item = item.strip()
+
+                    if item.startswith("DNS:"):
+                        san_entries.append(
+                            x509.DNSName(item[4:])
+                        )
+                    elif item.startswith("IP:"):
+                        import ipaddress
+                        san_entries.append(
+                            x509.IPAddress(
+                                ipaddress.ip_address(item[3:])
+                            )
+                        )
+
+                if san_entries:
+                    cert_builder = cert_builder.add_extension(
+                        x509.SubjectAlternativeName(san_entries),
+                        critical=False,
+                    )
+
+            cert = cert_builder.sign(
+                private_key=key,
+                algorithm=hashes.SHA512(),
+            )
+
+            with open(CERT_FILE, "wb") as f:
+                f.write(
+                    cert.public_bytes(
+                        serialization.Encoding.PEM
+                    )
+                )
+
+            with open(KEY_FILE, "wb") as f:
+                f.write(
+                    key.private_bytes(
+                        encoding=serialization.Encoding.PEM,
+                        format=serialization.PrivateFormat.TraditionalOpenSSL,
+                        encryption_algorithm=serialization.NoEncryption(),
+                    )
+                )
+
+            return 1
+
+        except Exception as e:
+            print(e)
+            return 0
 
     def set_tls_context(self, reply):
         kwargs= {}
@@ -108,7 +170,7 @@ class TlsServer():
             self.context.load_cert_chain(self.cert_file, self.key_file)
             return 1
         return 0
-            
+
 
     def accept(self):
         self.check_server()
